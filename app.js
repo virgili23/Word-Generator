82710a1687ae1ee7f69da851043cf34bb6bcf40c
virgil23 <virgil17@live.com>
2019-01-10 00:05:32 -0500

    removed reset button, now just click the word squares to change them

---
+++ app.js
@@ -0,0 +1,15 @@
+// Notes
+
+// May I look at your site? 
+
+// Website: Sunflowerwithin.com
+
+// webstart
+
+// Is it through a service like Squarespace, Wix or Wordpress? 
+
+// I have never worked with those, so I may have to look into them before I give you an answer 
+
+// I can add Simple ecommerce through Shopify 
+
+// What would you like done to it? 
